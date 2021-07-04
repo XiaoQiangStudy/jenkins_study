78295657df0fbfd9f46aa4ca514dda2b00cb9a3d
Jayhoo <hellohsq@163.com>
2021-07-04 14:55:05 +0800

    00

--- run.sh
+++ run.sh
@@ -2,18 +2,18 @@ mkdir -p webroot
 if [ -a "webroot/demo.pid" ]; then
 pid=$(cat webroot/demo.pid)
 existpid=$(ps aux | awk '{print $2}'| grep -w $pid)
-@echo "获得PID=$pid, 运行中PID=$existpid"
+echo "获得PID=$pid, 运行中PID=$existpid"
 fi
 if [[ $existpid != "" ]]; then
 kill -9 $pid
-@echo "目标 $pid 进程存在，已删除之"
+echo "目标 $pid 进程存在，已删除之"
 fi
 if [ -a "build/libs/demo-0.0.1-SNAPSHOT.jar" ]; then
 mv -f build/libs/demo-0.0.1-SNAPSHOT.jar webroot/demo.jar
-@echo "jar包已拷贝"
+echo "jar包已拷贝"
 fi
 cd webroot
 if [ -a demo.jar ]; then
 nohup java -jar demo.jar > demo.log 2>&1 & echo $! >"demo.pid"
-@echo "已执行发布"
+echo "已执行发布"
 fi
